b3ccd8d531b7c13b0f4fc90faa9b53508485089e
ItSparten <99556198+ItSparten@users.noreply.github.com>
2024-06-30 19:56:42 +0200

    Update CozyApplicationTests.java

--- src/test/java/com/cozy/CozyApplicationTests.java
+++ src/test/java/com/cozy/CozyApplicationTests.java
@@ -2,8 +2,12 @@ package com.cozy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootTest
+@PropertySource("classpath:application.properties")
+@EnableJpaRepositories(basePackages = "com.cozy")
 class CozyApplicationTests {
 
     @Test
